ad8473cf78ae36a4a51529fdceb9856bb99e9ea2
Ronaldo Santos <ronaldo.tboupt@gmail.com>
2024-08-04 18:24:55 -0300

    Criação de projeto

---
+++ src/test/java/com/agendamento/projetoagendamento/ProjetoagendamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agendamento.projetoagendamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoagendamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
